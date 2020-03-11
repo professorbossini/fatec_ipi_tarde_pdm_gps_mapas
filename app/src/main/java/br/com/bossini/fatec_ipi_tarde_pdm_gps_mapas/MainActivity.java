3f9887637865513c1adbcfcea93b4febea68d8d3
Rodrigo Bossini <professorbossini@gmail.com>
2020-03-11 16:26:38 -0300

    Terminamos o pdf.

--- app/src/main/java/br/com/bossini/fatec_ipi_tarde_pdm_gps_mapas/MainActivity.java
+++ app/src/main/java/br/com/bossini/fatec_ipi_tarde_pdm_gps_mapas/MainActivity.java
@@ -1,25 +1,43 @@
 package br.com.bossini.fatec_ipi_tarde_pdm_gps_mapas;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URI;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int GPS_REQ_CODE = 1001;
+
     private LocationManager locationManager;
     private LocationListener locationListener;
+
+    private TextView locationTextView;
+
+    private double latitude;
+    private double longitude;
 
     private void configurarGPS (){
        locationManager =
@@ -28,7 +46,16 @@ public class MainActivity extends AppCompatActivity {
        locationListener = new LocationListener() {
            @Override
            public void onLocationChanged(Location location) {
-
+               latitude = location.getLatitude();
+               longitude = location.getLongitude();
+               String mostrar =
+                       String.format(
+                               Locale.getDefault(),
+                               "Lat: %f, Long: %f",
+                               latitude,
+                               longitude
+                       );
+               locationTextView.setText(mostrar);
            }
 
            @Override
@@ -51,38 +78,91 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationTextView = findViewById(R.id.locationTextView);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        configurarGPS();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Uri uri = Uri.parse(
+                        String.format(
+                                Locale.getDefault(),
+                                "geo:%f, %f?q=restaurantes",
+                                latitude,
+                                longitude
+                        )
+                );
+                Intent intent= new Intent(
+                        Intent.ACTION_VIEW,
+                        uri
+                );
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
             }
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION)
+             == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    1000,
+                    5,
+                    locationListener
+            );
+        }
+        else{
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String [] {
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    GPS_REQ_CODE
+            );
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[]
+                                                   permissions,
+                                           @NonNull int[]
+                                                   grantResults) {
+        if (requestCode == GPS_REQ_CODE){
+            //resposta pelo gps
+            if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            1000,
+                            5,
+                            locationListener
+                    );
+                }
+            }
+            else{
+                Toast.makeText(
+                        this, getString(R.string.no_gps_no_app),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
